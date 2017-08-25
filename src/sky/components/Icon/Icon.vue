644b6134939079b98d8c43f805501266b9da73be
whynodejs <whynodejs@gmail.com>
2017-08-25 23:09:46 +0800

    <feature>(icon) add Icon.vue

---
+++ src/sky/components/Icon/Icon.vue
@@ -0,0 +1,31 @@
+<template>
+  <i :class='classes' :style='styles'></i>  
+</template>
+<script>
+const prefix = 'sky-icon'
+export default{
+  name: 'Icon',
+  props: {
+    type: {
+      type: String,
+      required: true
+    },
+    size: {
+      type: Number,
+      default: 14
+    },
+    color: String
+  },
+  computed: {
+    classes () {
+      return `${prefix} ${prefix}-${this.type}`
+    },
+    styles () {
+      let styles = {}
+      styles['font-size'] = this.size + 'px'
+      styles['color'] = this.color
+      return styles
+    }
+  }
+}  
+</script>
