a758c148884714e157cab29dd8aea85b9372fad8
whynodejs <whynodejs@gmail.com>
2017-08-29 00:16:08 +0800

    modify styles

--- src/sky/index.js
+++ src/sky/index.js
@@ -2,13 +2,16 @@ import Button from './components/Button'
 import Icon from './components/Icon'
 import Input from './components/Input'
 import Modal from './components/Modal'
+import {Row, Col} from './components/layout'
 import './styles/index.less'
 
 let sky = {
   Button,
   Icon,
   Input,
-  Modal
+  Modal,
+  Row,
+  Col
 }
 
 const install = function (Vue) {
