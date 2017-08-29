b57e9d7797fd2264a52043f0e9def555866c4431
whynodejs <whynodejs@gmail.com>
2017-08-30 00:10:37 +0800

    add card component

---
+++ src/sky/components/Card/Card.vue
@@ -0,0 +1,44 @@
+<template>
+  <div :class='wrapClass' :style='styles'>
+    <div class='sky-card-header' v-if='showHeader'>
+      <slot name='header'></slot>
+    </div>
+    <div class='sky-card-content'>
+      <slot name='content'></slot>
+    </div>
+    <slot></slot>
+  </div>
+</template>
+<script>
+const prefix = 'sky-card'
+export default{
+  name: 'Card',
+  props: {
+    shadow: {
+      type: Boolean,
+      default: true
+    },
+    height: Number
+  },
+  data () {
+    return {
+      showHeader: false
+    }
+  },
+  mounted () {
+
+  },
+  computed: {
+    wrapClass () {
+      return [
+        `${prefix}`, {
+          [`${prefix}-shadow`]: this.shadow
+        }
+      ]
+    },
+    styles () {
+      return this.height ? {'height': this.height} : ''
+    }
+  }
+}
+</script>
--- src/sky/index.js
+++ src/sky/index.js
@@ -2,6 +2,10 @@ import Button from './components/Button'
 import Icon from './components/Icon'
 import Input from './components/Input'
 import Modal from './components/Modal'
+import CheckBox from './components/CheckBox'
+import {Radio, RadioGroup} from './components/Radio'
+import SSwitch from './components/Switch'
+import Card from './components/Card'
 import {Row, Col} from './components/layout'
 import './styles/index.less'
 
@@ -11,7 +15,12 @@ let sky = {
   Input,
   Modal,
   Row,
-  Col
+  Col,
+  CheckBox,
+  Radio,
+  RadioGroup,
+  SSwitch,
+  Card
 }
 
 const install = function (Vue) {
